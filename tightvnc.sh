ef8134be30374a463764567ce328a7b98dee9c4e
lemotnet <69942707+lemotnet@users.noreply.github.com>
2020-12-25 19:14:06 +0700

    Add files via upload

---
+++ tightvnc.sh
@@ -0,0 +1,6 @@
+echo "===================================="
+echo "Selamat Datang di Fawzya.Net"
+echo "===================================="
+apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base
+apt-get install -y tightvncserver
+tightvncserver :1
---
+++ xrdp.sh
@@ -0,0 +1,9 @@
+echo "===================================="
+echo "Selamat Datang di Fawzya.Net"
+echo "===================================="
+sudo apt-get update -y 
+sudo apt-get install -y xfce4 xfce4-goodies
+sudo apt-get install -y firefox
+sudo apt-get install -y xrdp
+echo xfce4-session >~/.xsession
+sudo service xrdp start
